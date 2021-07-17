5cca173c61fb028a5322146addcc39dc669cb31f
GaboGomezT <gomezgabriel09@gmail.com>
2021-07-17 15:27:44 -0500

    Fast API webapp template fully setup

---
+++ views/home.py
@@ -0,0 +1,13 @@
+import fastapi
+from fastapi_chameleon import template
+
+router = fastapi.APIRouter()
+
+
+@router.get('/')
+@template()
+def index():
+    return {
+        "message": "Hello World!"
+    }
+
