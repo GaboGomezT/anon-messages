0f9fed6ba94195e93351f82ced2cd48c0ddc68cc
Gabriel <gomezgabriel09@gmail.com>
2023-02-13 23:30:58 -0600

    db string fix

--- infrastructure/mongo.py
+++ infrastructure/mongo.py
@@ -10,10 +10,13 @@ HOST = getenv("host")
 PORT = getenv("port")
 DATABASE = getenv("port")
 PROTOCOL = getenv("protocol")
+DB_URL = getenv("db_url")
 
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
 cert_path = path.join(getcwd(),"ca-certificate.crt")
 engine_string = f"{PROTOCOL}://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}?authSource=admin&replicaSet=mongo-db-crud&tls=true&tlsCAFile={cert_path}"
+if DB_URL:
+    engine_string = DB_URL
 client = MongoClient(engine_string)
 
 db = client.admin
